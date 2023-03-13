19737302ff40d20d21608b79b4e29cae398fb9c0
unknown <ox7c00@foxmail.com>
2023-03-13 17:58:07 +0800

    test

---
+++ sum.go
@@ -0,0 +1,13 @@
+package calculator
+
+var logMessage = "[LOG]"
+
+var Version = "1.0"
+
+func internalSum(number int) int {
+	return number + 1
+}
+
+func Sum(num1, num2 int) int {
+	return num1 + num2
+}
